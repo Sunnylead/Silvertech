6a3aa65e85a8e7058f3d99488de84e9362474e65
Ameh Sunday <106314710+Sunnylead@users.noreply.github.com>
2022-11-30 05:00:23 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,28 @@
+// //  NAV-BAR
+//    window.onscroll = function() {myFunction()};
+
+//    var navbar = document.getElementById("goat");
+//    var sticky = navbar.offsetTop;
+
+//   function myFunction() {
+//    if (window.pageYOffset >= sticky) {
+//         goat.classList.add("sticky")
+//   } 
+//   else {
+//       goat.classList.remove("sticky");
+//   }
+//  }
+
+let javA = document.getElementById("java");
+let box = document.getElementById("change");
+
+ function changecolor(){
+    if( box.style.background == "red"){
+          box.style.background = "blue";
+    } 
+
+    else{
+        box.style.background = "red";
+    }
+    }
+   
